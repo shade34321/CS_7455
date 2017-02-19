8f48683b62eeb0dcf0514e08bc0b3d747d58cf21
Shade Alabsa <shade34321@gmail.com>
2017-02-19 13:35:15 -0500

    Script for part two of the homework assignment.

--- hw01/soccer_picture.m
+++ hw01/soccer_picture.m
@@ -1,23 +1,45 @@
-addpath E:\Dropbox\School\SPSU_KSU\CS_7455\CS_7455\hw01;
-cd E:\Dropbox\School\SPSU_KSU\CS_7455\CS_7455\hw01;
-soccer = imread('soccer_field4.jpg');
-color = double(soccer);
+% The next two lines were for me in testing. Just put the filename into the
+% variable filename and it should work.
+%addpath E:\Dropbox\School\SPSU_KSU\CS_7455\CS_7455\hw01;
+%cd E:\Dropbox\School\SPSU_KSU\CS_7455\CS_7455\hw01;
+filename = 'E:\Dropbox\School\SPSU_KSU\CS_7455\CS_7455\hw01\data\soccer_field4.jpg';
+soccer = imread(filename); %read in the file.
+
+% Extract all three color bands into their own respective spots.
+color = double(soccer); 
 r = color(:,:, 1);
 g = color(:,:, 2);
 b = color(:,:, 3);
 
-red = ((r - g > 10) & (r-b > 10));
-green = ((g -r > 10) & (g - b > 10));
-blue = ((b - r > 10) & (b - g > 10));
-red = imdilate(red, ones(7,7));
-green = imdilate(green, ones(7,7));
-blue = imdilate(green, ones(7,7));
+% Identify all the green areas. Playing with this gives various results. 
+green = ((g - r > 50) & (g - b > 50));
+green = imdilate(green, ones(7,7)); %dilate the image a bit. Based off code from the slides.
+[labels, ~] = bwlabel(green, 4);
+field = (labels == 2);
+field = remove_holes(field);
 
-green = remove_holes(green);
+% Here we filter out some more noise and then identify all the red and blue
+% areas
+green = imclose(green, ones(7,7));
+green = imerode(green, ones(7,7));
+red = ((r - g > 100) & (r - b > 100));
+blue = ((b - r > 50) & (b - g > 50));
 
-%[labels, ~] = bwlabel(green, 4);
-%field = (labels == 2);
-%field = remove_holes(field);
+% Since we dilated the green the green is also included with the red and
+% blue section.
+% I didn't dilate the red and blue since it seemed to make blocks and
+% return an ugly result.
+red_players = (red & green);
+blue_players = (blue & green);
+
+% Display the field
 figure(1);
-imshow(field)%
+imshow(field);
 
+% Display the red players
+figure(2);
+imshow(red_players);
+
+% Display the blue players
+figure(3);
+imshow(blue_players);
