4548298ca7f1144c04991e17fc09fc4c30838d43
Shade Alabsa <shade34321@gmail.com>
2017-02-26 14:29:17 -0500

    WIP

--- hw02/face_detection/app/src/main/java/net/ophuk/facedetection/MainActivity.java
+++ hw02/face_detection/app/src/main/java/net/ophuk/facedetection/MainActivity.java
@@ -1,14 +1,10 @@
 package net.ophuk.facedetection;
 
+import android.app.Activity;
 import android.content.Context;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.util.Log;
 import android.view.WindowManager;
-
 import org.opencv.android.*;
 import org.opencv.android.CameraBridgeViewBase.CvCameraViewListener;
 import org.opencv.core.*;
@@ -19,7 +15,7 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.InputStream;
 
-public class MainActivity extends AppCompatActivity implements CvCameraViewListener {
+public class MainActivity extends Activity implements CvCameraViewListener {
 
     private CameraBridgeViewBase openCvCameraView;
     private CascadeClassifier cascadeClassifier;
@@ -29,24 +25,31 @@ public class MainActivity extends AppCompatActivity implements CvCameraViewListe
     private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
         @Override
         public void onManagerConnected(int status) {
-            switch (status){
+            Log.d("Shade", "Running onManagerConnected");
+            switch (status) {
                 case LoaderCallbackInterface.SUCCESS:
+                    Log.d("Shade", "LoaderCallbackInterface succeeded.");
                     initializeOpenCVDependencies();
                     break;
                 default:
+                    Log.d("Shade", "LoaderCallbackInterface did not succeeded.");
                     super.onManagerConnected(status);
                     break;
             }
         }
     };
 
-    private void initializeOpenCVDependencies(){
-        try{
+    private void initializeOpenCVDependencies() {
+
+        Log.d("Shade", "initializeOpenCVDependencies running.");
+
+        try {
             // Copy the resource into a temp file so OpenCV can load it
             InputStream is = getResources().openRawResource(R.raw.lbpcascade_frontalface);
-            File cascadeDir = getDir("cascade", Context.MODE_APPEND);
+            File cascadeDir = getDir("cascade", Context.MODE_PRIVATE);
             File mCascadeFile = new File(cascadeDir, "lbpcascade_frontalface.xml");
             FileOutputStream os = new FileOutputStream(mCascadeFile);
+
 
             byte[] buffer = new byte[4096];
             int bytesRead;
@@ -67,50 +70,34 @@ public class MainActivity extends AppCompatActivity implements CvCameraViewListe
     }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
+        Log.d("Shade", "onCreate running.");
         super.onCreate(savedInstanceState);
 
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
         openCvCameraView = new JavaCameraView(this, -1);
         setContentView(openCvCameraView);
         openCvCameraView.setCvCameraViewListener(this);
-        //setContentView(R.layout.activity_main);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
     }
 
     @Override
     public void onCameraViewStarted(int width, int height) {
+        Log.d("Shade", "onCameraaViewStarted running.");
         grayscaleImage = new Mat(height, width, CvType.CV_8UC4);
+
         // The faces will be a 20% of the height of the screen
         absoluteFaceSize = (int) (height * 0.2);
     }
 
     @Override
     public void onCameraViewStopped() {
-
+        Log.d("Shade", "onCameraViewStopped running.");
     }
 
     @Override
     public Mat onCameraFrame(Mat aInputFrame) {
+        Log.d("Shade", "onCameraFrame running.");
         // Create a grayscale image
         Imgproc.cvtColor(aInputFrame, grayscaleImage, Imgproc.COLOR_RGBA2RGB);
 
@@ -118,19 +105,27 @@ public class MainActivity extends AppCompatActivity implements CvCameraViewListe
 
         // Use the classifier to detect faces
         if (cascadeClassifier != null) {
+            Log.d("Shade", "cascadeClassifier is not null");
             cascadeClassifier.detectMultiScale(grayscaleImage, faces, 1.1, 2, 2,
                     new Size(absoluteFaceSize, absoluteFaceSize), new Size());
         }
 
         // If there are any faces found, draw a rectangle around it
         Rect[] facesArray = faces.toArray();
-        for (int i = 0; i <facesArray.length; i++)
+        Log.d("Shade", "facesArray.length: " + facesArray.length);
+        for (int i = 0; i <facesArray.length; i++) {
+            Log.d("onCameraFrame", "Drawing rectangles around face.");
             Imgproc.rectangle(aInputFrame, facesArray[i].tl(), facesArray[i].br(), new Scalar(0, 255, 0, 255), 3);
+
+        }
+
 
         return aInputFrame;
     }
+
     @Override
     public void onResume() {
+        Log.d("Shade", "onResume running.");
         super.onResume();
         OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_2_4_6, this, mLoaderCallback);
     }
